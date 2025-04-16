5ec8fc78c02378238244a5bae6f3c27b656a9b6d
gait <store.goldenart@gmail.com>
2025-04-17 03:26:45 +0800

    refactor : changed the start_time code

--- app/app.py
+++ app/app.py
@@ -18,13 +18,11 @@ with st.form("add_staff"):
 # View staff
 
 st.subheader("All Staff")
-result_time = {}
-result_time["start_time"] = time.time()
+start_time = time.time()
 
 res = requests.get(f"{API_BASE}/staff")
 if res.ok:
     st.table(res.json())
 
-result_time["end_time"] = time.time()
-duration = round(result_time["end_time"] - result_time["start_time"], 2)
+duration = round(time.time() - start_time, 2)
 st.success(f"Reloaded in {duration} seconds")
